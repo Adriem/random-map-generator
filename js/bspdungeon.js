a9664b3dac9181a9a3c62490eeb4e5898c38307a
Adriem <admoreno@outlook.com>
2015-04-17 17:23:15 +0200

    Draw grid added

--- js/bspdungeon.js
+++ js/bspdungeon.js
@@ -1,5 +1,5 @@
 var MAP_SIZE = 64
-var TILE_SIZE = 400 / MAP_SIZE
+var TILE_SIZE = 400 / MAP_SIZE;
 var ITERATIONS = 5
 var RATIO_RESTR = 0.45
 var RATIO_RESTR_ENABLE = true
@@ -7,6 +7,7 @@ var SIZE_RESTR = 5
 var SIZE_RESTR_ENABLE = true
 var PARTITION_LEVEL = 2
 var SECTOR_MIN_SIZE = 0
+var ROOM_REDUCTION_RATIO = 3
 
 
 function randomValue(min, max) {
@@ -18,37 +19,58 @@ function randomTest(val) {
 	return Math.random() * 100 < val
 }
 
+var Rect = function(x, y, w, h) {
+    this.x = x; //X coordinate
+    this.y = y; //Y coordinate
+    this.w = w; //Width
+    this.h = h; //Height
+    this.center = {
+        x: this.x + this.w/2,
+        y: this.y + this.h/2 
+    }
+}
+
 var Sector = function () {
-	this.room = undefined
+    Rect.call(this, x, y, w, h);
+	this.room = undefined;
 }
-Sector.prototype.split = function() {
-	return this.room
-}
-/*Sector.prototype.splitStep = function (sector, horizontalDirection, steps) {
+Sector.prototype = Object.create(Rect.prototype);
+Sector.prototype.constructor = Sector;
+Sector.prototype.split = function (sector, horizontalDirection, steps) {
+	horizontalDirection = horizontalDirection !== undefined ? horizontalDirection : randomTest(50);
+	steps = steps !== undefined ? steps : PARTITION_LEVEL;
+	var div1, div2, restriction;
 	if (horizontalDirection) {
-		if(steps === 0 || (sector.h < 2*SECTOR_MIN_SIZE && SIZE_RESTR_ENABLE) || 
-				(sector.h/sector.w < 2*RATIO_RESTR && RATIO_RESTR_ENABLE) return [sector];
-		var div1, div2
-		do {
-			div1 = 
-		} while ((SIZE_RESTR_ENABLE && div1.h < SECTOR_MIN_SIZE && div2.h < SECTOR_MIN_SIZE) || 
-				(RATIO_RESTR_ENABLE && div1.h/div1.w < RATIO_RESTR && div2.h/div2.w < RATIO_RESTR))
+		if(steps === 0 || sector.h < 2*SECTOR_MIN_SIZE || 
+				sector.h/sector.w < 2*RATIO_RESTR) return [sector];
+		restriction = Math.max(SECTOR_MIN_SIZE, Math.ceil(sector.h*RATIO_RESTR));
+		div1 = new  Sector(sector.x, sector.y, sector.w, 
+				randomValue(restriction, sector.h - restriction));
+		div2 = new Sector(sector.x, sector.y + div1.h, sector.w, sector.h - div1.h);
 	} else {
-	
+		if(steps === 0 || sector.w < 2*SECTOR_MIN_SIZE || 
+				sector.w/sector.h < 2*RATIO_RESTR) return [sector];
+		restriction = Math.max(SECTOR_MIN_SIZE, Math.ceil(sector.w*RATIO_RESTR));
+		div1 = new  Sector(sector.x, sector.y, 
+				randomValue(restriction, sector.w - restriction, sector.h));
+		div2 = new Sector(sector.x + div1.w, sector.y, sector.w - div1.w, sector.h);
 	}
-
-
-
-	if ((SIZE_RESTR_ENABLE && (sector.w < SECTOR_MIN_SIZE || 
-			sector.h < SECTOR_MIN_SIZE)) || steps === 0) return [sector]
-	var div1, div2
-	if (horizontalDirection) { 		//HorizontalSplit
-		do {
-			div1 = new Sector(sector.x, sector.y, )
-		} while (RATIO_RESTR_ENABLE && (div1.h/div.w < RATIO_RESTR || 
-				div1.w/div1.h < RATIO_RESTR || div2/))
-	}
-}*/
+	return split(div1, !horizontalDirection, steps-1).concat(split(div2, !horizontalDirection, steps-1));
+}
+Sector.prototype.growRoom = function () {
+	var x, y, w, h
+	x = this.x + randomValue(1, Math.min(Math.floor(this.w/ROOM_REDUTCION_RATIO), 
+			Math.floor(this.h/ROOM_REDUCTION_RATIO)))
+	//y = this.y + random(0, Math.floor(this.h/3))
+	//w = this.w - (x - this.x)
+	//h = this.h - (y - this.y)
+	//w -= random(0, w/3)
+	//h -= random(0, h/3)
+	y = this.y + x - this.x
+	w = this.w - 2*(x - this.x)
+	h = this.h - 2*(y - this.y)
+	this.room = new Room(x, y, w, h)
+}
 
 var Tree = function(node) {
 	this.node = node
@@ -64,9 +86,9 @@ Object.defineProperty(Tree.prototype, "leafs", {
 		return retVal
 	}
 });
-Tree.prototype.grow = function (iterations) {
+Tree.prototype.grow = function (iterations, splitFunc) {
 	if (iterations !== 0) {
-		var childNodes = this.node.split()
+		var childNodes = splitFunc(this.node)
 		if(childNodes !== undefined) {
 			this.childs = []
 			for (var i=0; i<childNodes.length; i++) 
@@ -80,8 +102,13 @@ Tree.prototype.grow = function (iterations) {
 var DungeonMap = function(w, h) {
 	this.w = w;
 	this.h = h;
-	this.partitionTree = undefined
-	this.rooms = undefined
-	this.paths = undefined
-	this.tileMap = undefined
+	this.partitionTree = undefined;
+	this.rooms = undefined;
+	this.paths = undefined;
+	this.tileMap = undefined;
+
+	this.regenerate();
+}
+DungeonMap.prototype.regenerate = function () {
+	partitionTree = new Tree(new Sector(0,0,w,h))
 }
--- js/dungeon.js
+++ js/dungeon.js
@@ -1,11 +1,29 @@
+GRID_COLOR = "rgba(255, 255, 255, 0.4)"
+TILE_SIZE   = 400 / 64
+DRAW_GRID = true;
+
 var TileMap = function(w, h, c) {
 	this.w = w;
 	this.h = h;
 	this.c = c;
 
-	this.clear(this.c)
+	this.clear(this.c);
+	if (DRAW_GRID) this.drawGrid();
 }
 TileMap.prototype.clear = function (c) {
-	c.fillStyle = "#000"
-	c.fillRect(0, 0, this.w*TILE_SIZE, this.h*TILE_SIZE)
+	this.c.fillStyle = "#000"
+	this.c.fillRect(0, 0, this.w*TILE_SIZE, this.h*TILE_SIZE)
+}
+TileMap.prototype.drawGrid = function() {
+    this.c.beginPath();
+    this.c.strokeStyle = GRID_COLOR;
+    this.c.lineWidth = 0.5
+    for (var i = 0; i < MAP_SIZE; i++) {
+        this.c.moveTo(i * TILE_SIZE, 0)
+        this.c.lineTo(i * TILE_SIZE, MAP_SIZE * TILE_SIZE)
+        this.c.moveTo(0, i * TILE_SIZE)
+        this.c.lineTo(MAP_SIZE * TILE_SIZE, i * TILE_SIZE)
+    }
+    this.c.stroke()
+    this.c.closePath()
 }
